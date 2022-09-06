5f41d939ddcf95402d0795f2301a0467a1cc76bf
dario schaffner <darioschaffner7@gmail.com>
2022-09-06 11:12:47 +0200

    removed warnings

--- M-326_backend/M-326_backend/WeatherForecast.cs
+++ M-326_backend/M-326_backend/WeatherForecast.cs
@@ -1,13 +1,12 @@
-namespace M_326_backend
+namespace M_326_backend;
+
+public class WeatherForecast
 {
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
+    public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
+    public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-        public string? Summary { get; set; }
-    }
+    public string? Summary { get; set; }
 }
