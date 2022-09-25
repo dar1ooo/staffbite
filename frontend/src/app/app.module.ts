23bcd77f35d23af2c267a633d0b812b8eac815fc
dario <darioschaffner7@gmail.com>
2022-09-25 13:47:52 +0200

    removed register page, added login form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
     DashboardComponent,
     PageNotFoundComponent,
     HomeComponent,
--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -9,4 +9,29 @@ export class LoginComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+  public changeForm() {
+    let switchCtn = document.querySelector('#switch-cnt');
+    let switchC1 = document.querySelector('#switch-c1');
+    let switchC2 = document.querySelector('#switch-c2');
+    let switchCircle = document.querySelectorAll('.switch__circle');
+    let switchBtn = document.querySelectorAll('.switch-btn');
+    let aContainer = document.querySelector('#a-container');
+    let bContainer = document.querySelector('#b-container');
+    let allButtons = document.querySelectorAll('.submit');
+
+    switchCtn?.classList.add('is-gx');
+    setTimeout(function () {
+      switchCtn?.classList.remove('is-gx');
+    }, 1500);
+
+    switchCtn?.classList.toggle('is-txr');
+    switchCircle[0].classList.toggle('is-txr');
+    switchCircle[1].classList.toggle('is-txr');
+
+    switchC1?.classList.toggle('is-hidden');
+    switchC2?.classList.toggle('is-hidden');
+    aContainer?.classList.toggle('is-txl');
+    bContainer?.classList.toggle('is-txl');
+    bContainer?.classList.toggle('is-z200');
+  }
 }
