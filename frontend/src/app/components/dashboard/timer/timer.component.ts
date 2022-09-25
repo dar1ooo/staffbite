349539be4a8ef24114b10b6dc71284f7f953b6bb
dario <darioschaffner7@gmail.com>
2022-09-25 20:20:36 +0200

    WIP dashboard

--- frontend/src/app/components/dashboard/timer/timer.component.ts
+++ frontend/src/app/components/dashboard/timer/timer.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
-  styleUrls: ['./timer.component.scss']
+  styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
