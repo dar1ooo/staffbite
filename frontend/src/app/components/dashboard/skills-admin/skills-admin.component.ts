1fbac009c9bb6797eaab230fc6fc6238949118f0
dario <darioschaffner7@gmail.com>
2022-11-02 22:22:38 +0100

    added ability to create new skills with video and pdf links

--- frontend/src/app/components/dashboard/skills-admin/skills-admin.component.ts
+++ frontend/src/app/components/dashboard/skills-admin/skills-admin.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { defaultUrlMatcher } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, tap } from 'rxjs';
 import { Skill, Skills, User } from 'src/app/models';
+import { SubSkill } from 'src/app/models/subskill.model';
 import { SKillsService } from 'src/app/services/skills.service';
 
 @Component({
@@ -15,6 +17,11 @@ export class SkillsAdminComponent implements OnInit {
 
   public currentSkills: Skills[] = [];
   public newSkill: Skills = new Skills();
+  public beginnerDescription: string = '';
+  public advancedDescription: string = '';
+  public expertDescription: string = '';
+  public pdfLink: string = '';
+  public videoLink: string = '';
   public activeSkill: number = 1;
 
   constructor(
@@ -24,18 +31,10 @@ export class SkillsAdminComponent implements OnInit {
 
   ngOnInit(): void {
     this.newSkill.SkillTopic = '';
-    this.newSkill.Skills.push({
-      SubSkills: [
-        {
-          Description: 'SubSkill 1',
-          IsChecked: false,
-          ShowPdf: false,
-          PdfUrl: '',
-          ShowVideo: false,
-          VideoUrl: '',
-        },
-      ],
-    });
+    this.newSkill.Skills = [];
+    this.newSkill.Skills.push(new Skill());
+    this.newSkill.Skills.push(new Skill());
+    this.newSkill.Skills.push(new Skill());
 
     let mockedSkills = new Skills();
     mockedSkills = {
@@ -116,84 +115,139 @@ export class SkillsAdminComponent implements OnInit {
 
   public saveSkill(): void {
     if (this.skillFormValid()) {
-      this.skillService
-        .saveNewSkill(this.user, this.currentSkills)
-        .pipe(
-          tap((data) => {
-            this.toastr.success('Skill saved successfully');
-            this.resetSkillForm();
-          }),
-          catchError((err) => {
-            this.toastr.error('Error saving skill');
-            return err;
-          })
-        )
-        .subscribe();
+      this.currentSkills.push(this.newSkill);
+      this.resetSkillForm();
+      // this.skillService
+      //   .saveNewSkill(this.user, this.currentSkills)
+      //   .pipe(
+      //     tap((data) => {
+      //       this.toastr.success('Skill saved successfully');
+      //     }),
+      //     catchError((err) => {
+      //       this.toastr.error('Error saving skill');
+      //       return err;
+      //     })
+      //   )
+      //   .subscribe();
     }
   }
 
+  public addSubSkill(): void {
+    let subSkill = new SubSkill();
+
+    switch (this.activeSkill) {
+      case 1:
+        if (
+          this.beginnerDescription === null ||
+          this.beginnerDescription === ''
+        ) {
+          return;
+        }
+
+        if (this.pdfLink !== null && this.pdfLink !== '') {
+          subSkill.ShowPdf = true;
+          subSkill.PdfUrl = this.pdfLink;
+        }
+        if (this.videoLink !== null && this.videoLink !== '') {
+          subSkill.ShowVideo = true;
+          subSkill.VideoUrl = this.videoLink;
+        }
+
+        subSkill.Description = this.beginnerDescription;
+        this.newSkill.Skills[0].SubSkills.push(subSkill);
+        this.beginnerDescription = '';
+        break;
+
+      case 2:
+        if (
+          this.advancedDescription === null ||
+          this.advancedDescription === ''
+        ) {
+          return;
+        }
+        if (this.pdfLink !== null && this.pdfLink !== '') {
+          subSkill.ShowPdf = true;
+          subSkill.PdfUrl = this.pdfLink;
+        }
+        if (this.videoLink !== null && this.videoLink !== '') {
+          subSkill.ShowVideo = true;
+          subSkill.VideoUrl = this.videoLink;
+        }
+
+        subSkill.Description = this.advancedDescription;
+        this.newSkill.Skills[1].SubSkills.push(subSkill);
+        this.advancedDescription = '';
+
+        break;
+      case 3:
+        if (this.expertDescription === null || this.expertDescription === '') {
+          return;
+        }
+        if (this.pdfLink !== null && this.pdfLink !== '') {
+          subSkill.ShowPdf = true;
+          subSkill.PdfUrl = this.pdfLink;
+        }
+        if (this.videoLink !== null && this.videoLink !== '') {
+          subSkill.ShowVideo = true;
+          subSkill.VideoUrl = this.videoLink;
+        }
+
+        subSkill.Description = this.expertDescription;
+        this.newSkill.Skills[2].SubSkills.push(subSkill);
+        this.expertDescription = '';
+
+        break;
+      default:
+        return;
+    }
+    this.pdfLink = '';
+    this.videoLink = '';
+  }
+
   private skillFormValid(): boolean {
-    // if (this.newSkill.SkillTopic === '' || this.newSkill.SkillTopic === null) {
-    //   this.toastr.error('Please enter a skill topic');
-    //   return false;
-    // } else if (
-    //   this.newSkill.Skills[0].Description === '' ||
-    //   this.newSkill.Skills[0].Description === null
-    // ) {
-    //   this.toastr.error('Please enter a skill description for skill 1');
-    //   return false;
-    // } else if (
-    //   this.newSkill.Skills[1].Description === '' ||
-    //   this.newSkill.Skills[1].Description === null
-    // ) {
-    //   this.toastr.error('Please enter a skill description for skill 2');
-    //   return false;
-    // } else if (
-    //   this.newSkill.Skills[2].Description === '' ||
-    //   this.newSkill.Skills[2].Description === null
-    // ) {
-    //   this.toastr.error('Please enter a skill description for skill 3');
-    //   return false;
-    // } else {
-    //   this.newSkill.SkillTopic = this.newSkill.SkillTopic.trim();
-    //   this.newSkill.Skills[0] = this.newSkill.Skills[0];
-    //   this.newSkill.Skills[1] = this.newSkill.Skills[1];
-    //   this.newSkill.Skills[2] = this.newSkill.Skills[2];
-    //   return true;
-    // }
-    return true;
+    if (this.newSkill.SkillTopic === '' || this.newSkill.SkillTopic === null) {
+      this.toastr.error('Please enter a skill topic');
+      return false;
+    } else if (this.newSkill.Skills[0].SubSkills.length === 0) {
+      this.toastr.error('Please enter at least one subskill for beginner');
+      return false;
+    } else if (this.newSkill.Skills[1].SubSkills.length === 0) {
+      this.toastr.error('Please enter at least one subskill for advanced');
+      return false;
+    } else if (this.newSkill.Skills[2].SubSkills.length === 0) {
+      this.toastr.error('Please enter at least one subskill for expert');
+      return false;
+    } else {
+      this.newSkill.SkillTopic = this.newSkill.SkillTopic.trim();
+      this.newSkill.Skills[0] = this.newSkill.Skills[0];
+      this.newSkill.Skills[1] = this.newSkill.Skills[1];
+      this.newSkill.Skills[2] = this.newSkill.Skills[2];
+      return true;
+    }
   }
 
   private resetSkillForm(): void {
-    // this.newSkill = new Skills();
-    // for (let i = 0; i < 3; i++) {
-    //   this.newSkill.Skills.push({
-    //     Description: '',
-    //     IsChecked: false,
-    //     ShowPdf: false,
-    //     PdfUrl: '',
-    //     ShowVideo: false,
-    //     VideoUrl: '',
-    //     SubSkills: [
-    //       {
-    //         Description: 'SubSkill 1',
-    //         IsChecked: false,
-    //         ShowPdf: false,
-    //         PdfUrl: '',
-    //         ShowVideo: false,
-    //         VideoUrl: '',
-    //       },
-    //     ],
-    //   });
-    // }
-    // this.activeSkill = 1;
+    this.newSkill = new Skills();
+    this.newSkill.SkillTopic = '';
+    this.newSkill.Skills = [];
+    this.newSkill.Skills.push(new Skill());
+    this.newSkill.Skills.push(new Skill());
+    this.newSkill.Skills.push(new Skill());
+    this.activeSkill = 1;
   }
 
   public changeSkill(skill: number): void {
     this.activeSkill = skill;
   }
 
-  public onKeydown(event): void {
-    event.preventDefault();
+  public openPdf(selectedSkill: SubSkill, e: Event) {
+    if (e && e.stopPropagation) e.stopPropagation();
+    window.open(selectedSkill.PdfUrl, '_blank');
+  }
+
+  public openVideo(selectedSkill: SubSkill, e: Event) {
+    if (e && e.stopPropagation) e.stopPropagation();
+
+    window.open(selectedSkill.VideoUrl, '_blank');
   }
 }
