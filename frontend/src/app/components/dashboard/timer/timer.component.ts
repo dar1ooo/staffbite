04c26a9c8733b56b8eb32278707a00b9be804062
dario <darioschaffner7@gmail.com>
2022-11-05 18:01:56 +0100

    implemented timer for teachers

--- frontend/src/app/components/dashboard/timer/timer.component.ts
+++ frontend/src/app/components/dashboard/timer/timer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CountdownConfig, CountdownEvent } from 'ngx-countdown';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-timer',
@@ -6,5 +8,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./timer.component.scss'],
 })
 export class TimerComponent {
-  constructor() {}
+  public time: number = null;
+  public showTimer: boolean = false;
+  public config: CountdownConfig = {
+    leftTime: this.time,
+    notify: [1],
+    format: 'HH:mm:ss',
+    prettyText: (text) => {
+      return text
+        .split(':')
+        .map((v) => `<span class="item">${v}</span>`)
+        .join('');
+    },
+  };
+
+  public showTimeUp: boolean = false;
+
+  constructor(private toastr: ToastrService) {}
+
+  public handleEvent(e: CountdownEvent) {
+    if (e.action.toUpperCase() === 'DONE') {
+      this.showTimeUp = true;
+    }
+  }
+
+  public back() {
+    this.showTimeUp = false;
+    this.showTimer = false;
+    this.time = this.config.leftTime = 0;
+  }
+
+  public begin() {
+    if (
+      this.time === 0 ||
+      this.time === null ||
+      this.time === undefined ||
+      this.time < 0
+    ) {
+      this.toastr.error('Please enter a time');
+    } else {
+      this.showTimer = true;
+    }
+  }
+
+  public configChanged() {
+    this.config.leftTime = this.time * 60;
+  }
 }
